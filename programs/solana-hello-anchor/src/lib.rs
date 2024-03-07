9fb912a6f29110b1c016cc08df19145434300b3f
luckychacha <358140284@qq.com>
2024-03-08 07:49:18 +0800

    Feat: Init Accounts

--- programs/solana-hello-anchor/src/lib.rs
+++ programs/solana-hello-anchor/src/lib.rs
@@ -7,9 +7,25 @@ pub mod solana_hello_anchor {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Counter Account Created");
+        msg!("Current Count: { }", counter.count);
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = payer_user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub payer_user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
